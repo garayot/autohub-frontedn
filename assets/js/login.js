4118a4923618b541d68cffef16f1eb08238421c9
garayot <garayot@gmail.com>
2023-12-24 21:48:17 +0800

    added login and register function

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,39 +1,36 @@
-function validateLogin() {
-  var email = document.getElementById("loginEmail").value;
-  var password = document.getElementById("loginPassword").value;
+// Login
+const form_login = document.getElementById("form_login");
 
-  // Perform your validation here
-  if (email === "") {
-      alert("Please enter your email.");
-  } else if (password === "") {
-      alert("Please enter your password.");
+form_login.onsubmit = async (e) => {
+  e.preventDefault();
+
+  document.querySelector("#form_login button").disabled = true;
+  document.querySelector(
+    "#form_login button"
+  ).innerHTML = `<div class="spinner-border me-2" role="status">
+                        </div>
+                        <span>Loading...</span>`;
+
+  const formData = new FormData(form_login);
+
+  const response = await fetch("http://webapp-backend.test/api/login", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+    body: formData,
+  });
+
+  let data;
+
+  if (response.ok) {
+    data = await response.json();
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("role", data.user.role);
+
+    window.location.pathname = "/index.html";
   } else {
-      // You can perform further validation or submit the form
-      // For demonstration purposes, redirect to main.html on successful login
-      window.location.href = "index.html";
+    data = await response.json();
+    throw new Error(data.message);
   }
-}
-
-function validateSignup() {
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var signupEmail = document.getElementById("signupEmail").value;
-  var signupPassword = document.getElementById("signupPassword").value;
-  var confirmPassword = document.getElementById("confirmPassword").value;
-
-  // Perform your validation here
-  if (firstName === "" || lastName === "") {
-      alert("Please enter your full name.");
-  } else if (signupEmail === "") {
-      alert("Please enter your email.");
-  } else if (signupPassword === "" || confirmPassword === "") {
-      alert("Please enter and confirm your password.");
-  } else if (signupPassword !== confirmPassword) {
-      alert("Passwords do not match.");
-  } else if (signupPassword.length < 8) {
-      alert("Password should be at least 8 characters long.");
-  } else {
-      // You can perform further validation or submit the form
-      alert("Signup successful!");
-  }
-}
+};
